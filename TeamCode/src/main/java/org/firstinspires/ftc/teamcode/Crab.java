d1b22be559428587926030c82c22325b712d847e
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2023-06-29 18:20:28 -0700

    Put the base motor mode in a variable.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Crab.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Crab.java
@@ -7,15 +7,15 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class Crab extends LinearOpMode {
-public double ticksToInches (int ticks){
-    double ticksPerRotation = 537.7;
-    double radius_Inches = 1.89;
-    double num_wheel_rotation = ticks/ticksPerRotation;
-    double distance = num_wheel_rotation * 2 * 3.14 * radius_Inches;
-    return distance;
-}
+    public double ticksToInches(int ticks) {
+        double ticksPerRotation = 537.7;
+        double radius_Inches = 1.89;
+        double num_wheel_rotation = ticks / ticksPerRotation;
+        return num_wheel_rotation * 2 * 3.14 * radius_Inches;
+    }
+
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
         // Declare our motors
         // Make sure your ID's match your configuration
         DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
@@ -23,10 +23,12 @@ public double ticksToInches (int ticks){
         DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRight = hardwareMap.dcMotor.get("backRight");
 
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        DcMotor.RunMode baseMode = DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+
+        frontLeft.setMode(baseMode);
+        backLeft.setMode(baseMode);
+        frontRight.setMode(baseMode);
+        backRight.setMode(baseMode);
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
         // frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -65,15 +67,15 @@ public double ticksToInches (int ticks){
             telemetry.addData("frontLeft", -ticksToInches(frontLeft.getCurrentPosition()));
             telemetry.addData("backLeft", -ticksToInches(backLeft.getCurrentPosition()));
             telemetry.update();
-            if(gamepad1.a){
+            if (gamepad1.a) {
                 frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                frontLeft.setMode(baseMode);
                 backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                backLeft.setMode(baseMode);
                 frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                frontRight.setMode(baseMode);
                 backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                backRight.setMode(baseMode);
             }
         }
     }
